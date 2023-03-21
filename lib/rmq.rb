2187799b931c46aa79134d2608fe7706fff792c2
sofinms <sofinms@gmail.com>
2023-03-21 14:47:08 +0400

    Update rmq.rb

--- lib/rmq.rb
+++ lib/rmq.rb
@@ -5,25 +5,37 @@ require 'bunny'
 
 module Parser
   class RMQ
-    APP_ID = 'begin_parser'
-    def self.get_channel
-      @@ch ||= begin
-        config = Parser::Config.get_config
+		APP_ID = 'uw_youtube'
+		def self.get_channel
+			@@ch ||= begin
+				config = O14::Config.get_config
 
-        conn = Bunny.new host: config.rmq['host'], port: config.rmq['port'], user: config.rmq['username'],
-         pass: config.rmq['password'], vhost: config.rmq['vhost']
-        conn.start
-        ch = conn.create_channel
-        ch.prefetch(1)
+				conn = Bunny.new :host => config.rmq['host'], :port => config.rmq['port'], :user => config.rmq['username'],
+				 :pass => config.rmq['password'], :vhost => config.rmq['vhost']
+				conn.start
+				ch = conn.create_channel
+				ch.prefetch(1)
 
-        at_exit { ch.close rescue nil }
+			  	at_exit { ch.close rescue nil }
 
-        ch
-      end
-    end
+			  	ch
+			end
+		end
+		
+		def self.create_channel config
+			conn = Bunny.new :host => config['host'], :port => config['port'], :user => config['username'],
+			 :pass => config['password'], :vhost => config['vhost']
+			conn.start
+			ch = conn.create_channel
+			ch.prefetch(1)
 
-    def self.send_message(exchage, message)
-      exchage.publish JSON.generate(message), persistent: true, app_id: APP_ID.to_sym, host: Socket.gethostname, content_type: 'application/json'
-    end
-  end
+		  	at_exit { ch.close rescue nil }
+
+		  	ch
+		end
+
+		def self.send_message(exchage, message)
+			exchage.publish JSON.generate(message), persistent: true, app_id: APP_ID.to_sym, host: Socket.gethostname, content_type: 'application/json'
+		end
+	end
 end
