4b8547b2faf0286541feb5dcaebd0863d5f663b4
sofinms <sofinms@gmail.com>
2023-03-21 14:49:12 +0400

    Update db.rb

--- lib/db.rb
+++ lib/db.rb
@@ -4,34 +4,59 @@ require 'mysql2'
 module Parser
   class DB
     def self.get_db
-      @db ||= begin
-        config = Parser::Config.get_config
+			@db ||= begin
+				config = O14::Config.get_config
 
-        db_connection_params = {
-          adapter: 'mysql2',
-          host: config.db['host'],
-          port: config.db['port'],
-          database: config.db['database'],
-          user: config.db['user'],
-          password: config.db['password'],
-          max_connections: 10,
-          encoding: 'utf8'
-        }
+				db_connection_params = {
+					 adapter: 'mysql2',
+					 host: config.db['host'],
+					 port: config.db['port'],
+					 database: config.db['database'],
+					 user: config.db['user'],
+					 password: config.db['password'],
+					 max_connections: 10,
+					 timeout: 30,
+			   		 encoding: 'utf8mb4'
+				}
 
-        db = Sequel.connect(db_connection_params)
+				db = Sequel.connect(db_connection_params)
 
-        db.extension(:connection_validator)
+			 db.extension(:connection_validator)
 
-        at_exit { disconnect }
+			 at_exit { disconnect }
 
-        db
-      end
-    end
+			 db
+			end
+		end
+		
+		def self.create_connection connection_params
+	      config = O14::Config.get_config
+	
+	      db_connection_params = {
+	        adapter: 'mysql2',
+	        host: connection_params['host'],
+	        port: connection_params['port'],
+	        database: connection_params['database'],
+	        user: connection_params['user'],
+	        password: connection_params['password'],
+	        max_connections: 10,
+	        encoding: 'utf8mb4'
+	      }
+	      
+	      if !connection_params['use_utf16'].nil?
+			  db_connection_params[:use_utf16] = connection_params['use_utf16']
+		  end
+	
+	      db = Sequel.connect(db_connection_params)
+	
+	       db.extension(:connection_validator)
+	
+	       at_exit { db.disconnect }
+	       db
+	    end
 
-    def self.disconnect
-      @db.disconnect
-      rescue StandardError
-      nil
-    end
+		def self.disconnect
+			@db.disconnect rescue nil
+		end
   end
 end
