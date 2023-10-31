02967e3256ac53114a7c2a17d51b9e4fb257435d
sofinms <sofinms@gmail.com>
2023-10-31 14:38:10 +0400

    fix code ladder in o14 exception_handler

--- lib/o14/exception_handler.rb
+++ lib/o14/exception_handler.rb
@@ -1,11 +1,11 @@
 module O14
-    class ExceptionHandler
-        @logger = O14::ProjectLogger.get_logger
+  class ExceptionHandler
+    @logger = O14::ProjectLogger.get_logger
 
-        def self.log_exception e, log_prefix = ''
-            log_prefix = "#{log_prefix} " if !log_prefix.empty?
-            @logger.error "#{log_prefix}#{ e.class.name } #{ e.message }"
-            @logger.error "Exception Backtrace: #{ e.backtrace.join("\n") }"
-        end
+    def self.log_exception e, log_prefix = ''
+      log_prefix = "#{log_prefix} " if !log_prefix.empty?
+      @logger.error "#{log_prefix}#{ e.class.name } #{ e.message }"
+      @logger.error "Exception Backtrace: #{ e.backtrace.join("\n") }"
     end
+  end
 end
